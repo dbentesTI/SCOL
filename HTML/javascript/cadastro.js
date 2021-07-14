0ca81207f8cccbed03bb4ca2c003dca9c7696a93
Feno-x <felipinhonet11@gmail.com>
2021-07-14 16:07:13 -0300

    inputs do cadastro receberam suas IDS e base do arquivo javascript referente ao cadastro criado

---
+++ HTML/javascript/cadastro.js
@@ -0,0 +1,5 @@
+function verificacao(cadastro) {
+    if (cadastro.Email.value=="" && cadastro.Senha.value=="" && cadastro.nome.value=="" && cadastro.sobrenome.value=="" && cadastro.celular.value=="") {
+        document.getElementById('email')
+    }
+}

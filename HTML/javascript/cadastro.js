38f1e2188f912d6d11f569345bf1c268efc97279
Feno-x <felipinhonet11@gmail.com>
2021-07-14 16:53:16 -0300

    valores dos inputs cadastro estão funcionando corretamente e sendo redirecionados para o LocalStorage

--- HTML/javascript/cadastro.js
+++ HTML/javascript/cadastro.js
@@ -1,5 +1,13 @@
-function verificacao(cadastro) {
-    if (cadastro.Email.value=="" && cadastro.Senha.value=="" && cadastro.nome.value=="" && cadastro.sobrenome.value=="" && cadastro.celular.value=="") {
-        document.getElementById('email')
-    }
+function verificacaocadastro(cadastro) {
+    const emaillogin = document.getElementById('email').value
+    localStorage.setItem('email', emaillogin)
+    const senhalogin = document.getElementById('senha').value
+    localStorage.setItem('senha', senhalogin)
+    const nomelogin = document.getElementById('nome').value
+    localStorage.setItem('nome', nomelogin)
+    const sobrenomelogin = document.getElementById('sobrenome').value
+    localStorage.setItem('sobrenome', sobrenomelogin)
+    const celularlogin = document.getElementById('celular').value
+    localStorage.setItem('celular', celularlogin)
+    window.location.href = "cadastroconfirmado.html"
 }

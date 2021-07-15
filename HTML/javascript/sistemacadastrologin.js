785921632047348fbe911d989afe1402c2dd3256
Feno-x <felipinhonet11@gmail.com>
2021-07-15 07:51:04 -0300

    arquivos js  referentes ao cadastro e login agora fazem parte de um só e os arquivos html referentes ao cadastro e login já estão linkados a esse novo arquivo js, essa commit possuí parte do desenvolvimento ainda não concluído do cadastro que manda o email e senha para a function verificacao(login)

--- HTML/javascript/sistemacadastrologin.js
+++ HTML/javascript/sistemacadastrologin.js
@@ -1,4 +1,4 @@
-function verificacaocadastro(cadastro) {
+function verificacaocadastro() {
     const emaillogin = document.getElementById('email').value
     localStorage.setItem('email', emaillogin)
     const senhalogin = document.getElementById('senha').value
@@ -11,3 +11,15 @@ function verificacaocadastro(cadastro) {
     localStorage.setItem('celular', celularlogin)
     window.location.href = "cadastroconfirmado.html"
 }
+function verificacao(login) {
+    const loginemail = +localStorage.getItem('email')
+    const loginsenha = +localStorage.getItem('senha')
+    if (login.Email.value == `${loginemail}`  && login.Senha.value == `${loginsenha}`) {
+    location = "index3.html"
+    }
+    else {
+        login.Email.value=""
+        login.Senha.value=""
+        alert('Email ou senha não encontrados no sistema.')
+    }
+    }
